50500da35585ec58ca98a4e3f9b5e8cf435bc306
Dangyi Liu <dangyi@google.com>
2025-07-20 23:19:38 -0700

    A lifted version of pallas_call that takes QArray inputs
    This makes it easier to feed QArray into pallas kernels.
    
    PiperOrigin-RevId: 785305169

---
+++ qwix/core/pallas.py
@@ -0,0 +1,216 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Support feeding QArray into Pallas kernels."""
+
+import copy
+import dataclasses
+import functools
+from typing import Any, Callable
+
+import jax
+from jax.experimental import pallas as pl
+import numpy as np
+from qwix.core import qarray
+
+
+def pallas_call(
+    kernel: Callable[..., None],
+    out_shape: Any,
+    *,
+    grid_spec: pl.GridSpec | None = None,
+    grid=(),
+    in_specs=pl.no_block_spec,
+    out_specs=pl.no_block_spec,
+    scratch_shapes=(),
+    **kwargs,
+) -> Callable[..., Any]:
+  """A lifted version of jax.pallas_call that takes QArray as arguments.
+
+  The specs for the QArray arguments should be the same as the original jax
+  arrays, and this function will take care of converting them properly.
+
+  Args:
+    kernel: same as in jax.pallas_call.
+    out_shape: same as in jax.pallas_call.
+    grid_spec: same as in jax.pallas_call.
+    grid: same as in jax.pallas_call.
+    in_specs: same as in jax.pallas_call. The specs for QArrays should stay the
+      same as the original jax arrays.
+    out_specs: same as in jax.pallas_call.
+    scratch_shapes: same as in jax.pallas_call.
+    **kwargs: extra arguments to pass to jax.pallas_call.
+
+  Returns:
+    A function that can be called on a number of positional array arguments to
+    invoke the Pallas kernel.
+  """
+  if grid_spec is None:
+    grid_spec = pl.GridSpec(grid, in_specs, out_specs, scratch_shapes)
+
+  def wrapper(*args):
+    in_specs = _update_block_specs_for_qarray(grid_spec.in_specs, args)
+    in_specs, args, restore_fn = _transform_block_specs_for_tpu(in_specs, args)
+
+    # pl.GridSpec doesn't support dataclasses.replace.
+    new_grid_spec = copy.copy(grid_spec)
+    new_grid_spec.in_specs = in_specs
+
+    # Range of the input_refs in the kernel arguments.
+    input_start = getattr(grid_spec, "num_scalar_prefetch", 0)
+    input_end = input_start + len(args)
+
+    return pl.pallas_call(
+        lambda *kernel_args: kernel(
+            *kernel_args[:input_start],
+            *restore_fn(kernel_args[input_start:input_end]),
+            *kernel_args[input_end:],
+        ),
+        out_shape,
+        grid_spec=new_grid_spec,
+        **kwargs,
+    )(*args)
+
+  return wrapper
+
+
+def _update_block_specs_for_qarray(block_specs: Any, args: Any) -> Any:
+  """Update block specs for QArray arguments."""
+
+  def _update_block_spec(spec: pl.BlockSpec, arg):
+    if not isinstance(arg, qarray.QArray):
+      return spec
+
+    # Calculate the number of blocks for each axis. We don't support uneven
+    # tiling for now.
+    if any(v % bv != 0 for v, bv in zip(arg.qvalue.shape, spec.block_shape)):
+      raise ValueError(f"{arg.qvalue.shape} % {spec.block_shape} != 0")
+    num_blocks = [v // bv for v, bv in zip(arg.qvalue.shape, spec.block_shape)]
+
+    # Scale down the block shape for the scale.
+    if any(s % b != 0 and s > 1 for s, b in zip(arg.scale.shape, num_blocks)):
+      raise ValueError(f"{arg.scale.shape} cannot be divided into {num_blocks}")
+    scale_block_shape = tuple(
+        s // b if s > 1 else 1 for s, b in zip(arg.scale.shape, num_blocks)
+    )
+    scale_index_map = lambda *a: tuple(
+        i if s > 1 else 0 for i, s in zip(spec.index_map(*a), arg.scale.shape)
+    )
+    scale_block_spec = dataclasses.replace(
+        spec, block_shape=scale_block_shape, index_map=scale_index_map
+    )
+    assert arg.zero_point is None, "Zero point is not supported yet."
+
+    return dataclasses.replace(arg, qvalue=spec, scale=scale_block_spec)
+
+  return jax.tree.map(_update_block_spec, block_specs, args)
+
+
+def _transform_block_specs_for_tpu(
+    block_specs: Any, args: Any
+) -> tuple[Any, Any, Callable[..., Any]]:
+  """Transform block specs and arguments so that they can be used on TPU.
+
+  This workarounds the Pallas TPU requirement that the block shapes must be
+  divisible by 8x128. A "restore" function is returned to restore the actual
+  unpadded block shapes inside the kernel.
+
+  Args:
+    block_specs: a pytree of pl.BlockSpec.
+    args: the arguments to the pallas_call, which have the same structure as
+      block_specs.
+
+  Returns:
+    A tuple of (new_block_specs, new_args, restore_fn), where new_block_specs
+    and new_args are the transformed pytrees, and restore_fn is a function that
+    needs to be called inside the kernel on the kernel arguments to restore the
+    actual block shapes.
+  """
+  flatten_block_specs, treedef = jax.tree.flatten(block_specs)
+  flatten_args = treedef.flatten_up_to(args)
+
+  # Information needed to restore the original block shapes inside the kernel.
+  # The keys are the indices of the arrays in the flattened pytree. A key may
+  # only appear in one of the dictionaries.
+  reverse_transposes = {}
+  reverse_reshapes = {}
+
+  for i, (spec, arg) in enumerate(zip(flatten_block_specs, flatten_args)):
+    # Check if the block shape is already divisible by 8x128.
+    if _can_fit_tpu_requirements(spec.block_shape, arg.shape):
+      continue
+
+    # Solution 1: try to transpose the array to put the longest axis at the end.
+    transpose = np.argsort(spec.block_shape)
+    block_shape_t = _reorder(spec.block_shape, transpose)
+    if _can_fit_tpu_requirements(block_shape_t, _reorder(arg.shape, transpose)):
+      flatten_args[i] = arg.transpose(transpose)
+      index_map_t = functools.partial(
+          lambda spec, transpose, *a: _reorder(spec.index_map(*a), transpose),
+          spec,
+          transpose,
+      )
+      flatten_block_specs[i] = dataclasses.replace(
+          spec, block_shape=block_shape_t, index_map=index_map_t
+      )
+      reverse_transposes[i] = np.argsort(transpose)
+      continue
+
+    # Solution 2: reshape the array into (*num_blocks, 1, prod(block_shape))).
+    # This satisfies the TPU requirement because the last two dimensions are
+    # equal to the respective dimension of the overall array.
+    dims = range(arg.ndim)
+    arg_t = qarray.split_axis(arg, dict(enumerate(spec.block_shape)))
+    arg_t = arg_t.transpose([2 * i for i in dims] + [2 * i + 1 for i in dims])
+    arg_t = arg_t.reshape([arg_t.shape[i] for i in dims] + [1, -1])
+    flatten_args[i] = arg_t
+    block_shape_t = tuple([1 for _ in dims] + [1, arg_t.shape[-1]])
+    index_map_t = functools.partial(
+        lambda spec, *a: spec.index_map(*a) + (0, 0), spec
+    )
+    flatten_block_specs[i] = dataclasses.replace(
+        spec, block_shape=block_shape_t, index_map=index_map_t
+    )
+    reverse_reshapes[i] = spec.block_shape
+
+  def restore(kernel_args):
+    flatten_kernel_args = treedef.flatten_up_to(kernel_args)
+    for i, kernel_arg in enumerate(flatten_kernel_args):
+      if i in reverse_transposes:
+        flatten_kernel_args[i] = kernel_arg.transpose(reverse_transposes[i])
+      elif i in reverse_reshapes:
+        # Note: reshape on MemoryRef may return corrupted data!
+        flatten_kernel_args[i] = kernel_arg[...].reshape(reverse_reshapes[i])
+    return treedef.unflatten(flatten_kernel_args)
+
+  return (
+      treedef.unflatten(flatten_block_specs),
+      treedef.unflatten(flatten_args),
+      restore,
+  )
+
+
+def _reorder(
+    sequence: tuple[Any, ...], order: tuple[int, ...]
+) -> tuple[Any, ...]:
+  """Reorder/transpose a sequence of elements."""
+  return tuple(sequence[i] for i in order)
+
+
+def _can_fit_tpu_requirements(
+    block_shape: tuple[int, ...], arg_shape: tuple[int, ...]
+) -> bool:
+  """Check if the block shape can fit the TPU requirements."""
+  return (block_shape[-1] % 128 == 0 or block_shape[-1] == arg_shape[-1]) and (
+      block_shape[-2] % 8 == 0 or block_shape[-2] == arg_shape[-2]
+  )
---
+++ tests/core/pallas_test.py
@@ -0,0 +1,262 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import functools
+
+from absl.testing import absltest
+from absl.testing import parameterized
+import jax
+from jax import numpy as jnp
+from jax.experimental import pallas as pl
+from jax.experimental.pallas import tpu as pltpu
+from qwix.core import dot_general
+from qwix.core import pallas
+from qwix.core import qarray
+
+
+class PallasTest(parameterized.TestCase):
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="channelwise",
+          block_shape=(128, 128),
+          qvalue_shape=(1024, 1024),
+          scale_shape=(1024, 1),
+          expected_scale_block_shape=(128, 1),
+          expected_scale_index_map={(1, 1): (1, 0)},
+      ),
+      dict(
+          testcase_name="blockwise",
+          block_shape=(256, 256),
+          qvalue_shape=(1024, 1024),
+          scale_shape=(32, 8),  # 32x128 tiling.
+          expected_scale_block_shape=(8, 2),
+          expected_scale_index_map={(1, 1): (1, 1)},
+      ),
+  )
+  def test_update_block_specs_for_qarray(
+      self,
+      block_shape,
+      qvalue_shape,
+      scale_shape,
+      expected_scale_block_shape,
+      expected_scale_index_map,
+  ):
+    block_spec = pl.BlockSpec(block_shape, lambda *args: tuple(args))
+    array = qarray.QArray(
+        qvalue=jax.ShapeDtypeStruct(qvalue_shape, jnp.int8),
+        scale=jax.ShapeDtypeStruct(scale_shape, jnp.float32),
+        zero_point=None,
+        qtype="int8",
+    )
+    new_block_spec = pallas._update_block_specs_for_qarray(block_spec, array)
+    self.assertIsInstance(new_block_spec, qarray.QArray)
+    self.assertEqual(new_block_spec.qvalue, block_spec)
+    self.assertIsInstance(new_block_spec.scale, pl.BlockSpec)
+    self.assertEqual(
+        new_block_spec.scale.block_shape, expected_scale_block_shape
+    )
+    for k, v in expected_scale_index_map.items():
+      self.assertEqual(new_block_spec.scale.index_map(*k), v)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="no_transform",
+          arg_shape=(4, 128),
+          block_shape=(4, 128),
+          expected_new_block_shape=(4, 128),
+      ),
+      dict(
+          testcase_name="transpose",
+          block_shape=(128, 8),
+          arg_shape=(128, 16),
+          expected_new_block_shape=(8, 128),
+      ),
+      dict(
+          testcase_name="reshape",
+          block_shape=(8, 8),
+          arg_shape=(32, 32),
+          expected_new_block_shape=(1, 1, 1, 64),
+      ),
+  )
+  def test_transform_block_specs_for_tpu(
+      self, block_shape, arg_shape, expected_new_block_shape
+  ):
+    block_spec = pl.BlockSpec(block_shape, lambda *args: tuple(args))
+    arg = jnp.zeros(arg_shape, dtype=jnp.float32)
+    new_block_spec, new_array, restore_fn = (
+        pallas._transform_block_specs_for_tpu(block_spec, arg)
+    )
+    self.assertTrue(
+        pallas._can_fit_tpu_requirements(
+            new_block_spec.block_shape, new_array.shape
+        )
+    )
+    self.assertEqual(new_block_spec.block_shape, expected_new_block_shape)
+    restored = restore_fn(
+        jnp.zeros(expected_new_block_shape, dtype=jnp.float32)
+    )
+    self.assertEqual(restored.shape, block_shape)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="tensorwise",
+          input_shape=(256, 256),
+          tiled_axes={},
+      ),
+      dict(
+          testcase_name="channelwise0",
+          input_shape=(256, 256),
+          tiled_axes={0: 1},
+      ),
+      dict(
+          testcase_name="channelwise1",
+          input_shape=(256, 256),
+          tiled_axes={1: 1},
+      ),
+      dict(
+          testcase_name="blockwise0",
+          input_shape=(256, 256),
+          tiled_axes={0: 128},
+      ),
+      dict(
+          testcase_name="blockwise1",
+          input_shape=(256, 256),
+          tiled_axes={1: 128},
+      ),
+      dict(
+          testcase_name="blockwise2",
+          input_shape=(256, 256),
+          tiled_axes={0: 128, 1: 128},
+      ),
+      dict(
+          testcase_name="channelwise_and_blockwise",
+          input_shape=(256, 256),
+          tiled_axes={0: 1, 1: 128},
+      ),
+      dict(
+          testcase_name="blockwise_and_channelwise",
+          input_shape=(256, 256),
+          tiled_axes={0: 128, 1: 1},
+      ),
+      dict(
+          testcase_name="3d",
+          input_shape=(4, 256, 256),
+          tiled_axes={1: 1, 2: 128},
+      ),
+  )
+  def test_pallas_dequantize(self, input_shape, tiled_axes):
+    """Comprehensive tests for the pallas_call function."""
+
+    def dequantize_kernel(q_ref, out_ref):
+      out_ref[...] = qarray.dequantize(jax.tree.map(lambda x: x[...], q_ref))
+
+    def dequantize_pallas(q: qarray.QArray):
+      block_shape = (1,) * (q.qvalue.ndim - 2) + (128, 128)
+
+      return pallas.pallas_call(
+          dequantize_kernel,
+          out_shape=jax.ShapeDtypeStruct(q.qvalue.shape, q.scale.dtype),
+          in_specs=[pl.BlockSpec(block_shape, lambda *args: args)],
+          out_specs=pl.BlockSpec(block_shape, lambda *args: args),
+          grid=tuple(i // bs for i, bs in zip(q.qvalue.shape, block_shape)),
+      )(q)
+
+    x = jax.random.uniform(jax.random.key(0), input_shape, jnp.float32)
+    how = qarray.HowToQuantize(
+        qtype="int8",
+        channelwise_axes=[],
+        tiled_axes=tiled_axes,
+        batch_axes=[],
+        scale_transpose=None,
+        calibration_method="absmax",
+    )
+    qx = qarray.quantize(x, how)
+    self.assertTrue(jnp.allclose(dequantize_pallas(qx), qarray.dequantize(qx)))
+
+  def test_pallas_matmul(self):
+    """A basic example of using Qwix pallas_call to implement matmul."""
+
+    def pallas_matmul_kernel(x_ref, y_ref, z_ref, acc_ref, *, nsteps):
+      @pl.when(pl.program_id(2) == 0)
+      def _():
+        acc_ref[...] = jnp.zeros_like(acc_ref)
+
+      x = jax.tree.map(lambda x: x[...], x_ref)
+      y = jax.tree.map(lambda x: x[...], y_ref)
+      acc_ref[...] += dot_general.dot_general(x, y, (((1,), (0,)), ((), ())))
+
+      @pl.when(pl.program_id(2) == nsteps - 1)
+      def _():
+        z_ref[...] = acc_ref[...].astype(z_ref.dtype)
+
+    def pallas_matmul(
+        x: qarray.QArray,
+        y: qarray.QArray,
+        *,
+        bm: int = 128,
+        bk: int = 128,
+        bn: int = 128,
+    ):
+      m, k = x.qvalue.shape
+      _, n = y.qvalue.shape
+
+      return pallas.pallas_call(
+          functools.partial(pallas_matmul_kernel, nsteps=k // bk),
+          out_shape=jax.ShapeDtypeStruct((m, n), jnp.float32),
+          grid=(m // bm, n // bn, k // bk),
+          in_specs=[
+              pl.BlockSpec((bm, bk), lambda i, j, k: (i, k)),
+              pl.BlockSpec((bk, bn), lambda i, j, k: (k, j)),
+          ],
+          out_specs=pl.BlockSpec((bm, bn), lambda i, j, k: (i, j)),
+          scratch_shapes=[pltpu.VMEM((bm, bn), jnp.float32)],
+          compiler_params=pltpu.CompilerParams(
+              dimension_semantics=("parallel", "parallel", "arbitrary")
+          ),
+      )(x, y)
+
+    x_how = qarray.HowToQuantize(
+        qtype="int8",
+        channelwise_axes=[],
+        tiled_axes={0: 1, 1: 128},
+        batch_axes=[],
+        scale_transpose=None,
+        calibration_method="absmax",
+    )
+    qx = qarray.quantize(
+        jax.random.uniform(jax.random.key(0), (256, 256), jnp.float32), x_how
+    )
+    y_how = qarray.HowToQuantize(
+        qtype="int8",
+        channelwise_axes=[],
+        tiled_axes={0: 128, 1: 1},
+        batch_axes=[],
+        scale_transpose=None,
+        calibration_method="absmax",
+    )
+    qy = qarray.quantize(
+        jax.random.uniform(jax.random.key(1), (256, 256), jnp.float32), y_how
+    )
+
+    self.assertTrue(
+        jnp.allclose(
+            pallas_matmul(qx, qy),
+            dot_general.dot_general(qx, qy, (((1,), (0,)), ((), ()))),
+        )
+    )
+
+
+if __name__ == "__main__":
+  absltest.main()
