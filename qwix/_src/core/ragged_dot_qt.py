1adee2f1f6d49c7c208300a9dcfaa483e2d2c9a5
Jiwon Shin <jiwonshin@google.com>
2025-10-09 13:36:23 -0700

    [QwixRaggedDotGeneral] Modify qwix `ragged_dot` forward and backward passes to call qwix `ragged_dot.ragged_dot_general`.
    - Add `ragged_dot.ragged_dot_general`.
    - Modify qwix `ragged_dot` to call qwix `ragged_dot.ragged_dot_general`.
    - Modify qwix `ragged_dot_qt` dlhs to call qwix `ragged_dot.ragged_dot`.
    - Modify qwix `ragged_dot_qt` drhs to call qwix `ragged_dot.ragged_dot_general`.
    
    PiperOrigin-RevId: 817317169

--- qwix/_src/core/ragged_dot_qt.py
+++ qwix/_src/core/ragged_dot_qt.py
@@ -17,7 +17,6 @@
 import dataclasses
 import functools
 import jax
-from jax import numpy as jnp
 from qwix._src import interception
 from qwix._src.core import qarray
 from qwix._src.core import ragged_dot
@@ -34,61 +33,6 @@ class RaggedDotQtConfig:
   # Backward pass settings
   dlhs_grad_qtype: jax.typing.DTypeLike | None = None
   drhs_grad_qtype: jax.typing.DTypeLike | None = None
-
-
-def _ragged_dot_general(
-    lhs,
-    rhs,
-    group_sizes,
-    dimension_numbers,
-    precision: jax.lax.PrecisionLike = None,
-    preferred_element_type: jax.typing.DTypeLike | None = None,
-    group_offset: jax.Array | None = None,
-):
-  """Quantized ragged_dot_general using in backward ragged_dot_qt.
-
-  In the ragged_dot_qt backward pass, quantization is applied to incoming
-  gradient only. In dlhs calculation, gradient(lhs of backward pass) has
-  lhs_scale of shape [M, 1]. In drhs calculation, gradient(rhs of backward pass)
-  has rhs_scale of shape [1, N].
-
-  Args:
-    lhs: The left-hand side array.
-    rhs: The right-hand side array.
-    group_sizes: An array specifying the sizes of the groups.
-    dimension_numbers: Dimension numbers for the ragged dot general.
-    precision: Optional precision for the dot product.
-    preferred_element_type: Optional type promotion for the output.
-    group_offset: Optional offset for group indices.
-
-  Returns:
-    The result of the ragged dot general operation.
-  """
-  lhs_val = lhs.qvalue if isinstance(lhs, qarray.QArray) else lhs
-  rhs_val = rhs.qvalue if isinstance(rhs, qarray.QArray) else rhs
-  lhs_scale = lhs.scale if isinstance(lhs, qarray.QArray) else None
-  rhs_scale = rhs.scale if isinstance(rhs, qarray.QArray) else None
-
-  preferred_element_type, result_type = qarray.get_accumulator_and_result_type(
-      lhs, rhs, preferred_element_type=preferred_element_type
-  )
-
-  out = jax.lax.ragged_dot_general(
-      lhs_val,
-      rhs_val,
-      group_sizes,
-      dimension_numbers,
-      precision=precision,
-      preferred_element_type=preferred_element_type,
-      group_offset=group_offset,
-  )
-
-  if lhs_scale is not None:  # [M, 1]
-    out = qarray.call_with_generic_broadcast(jnp.multiply, out, lhs_scale)
-  if rhs_scale is not None:  # [1, N]
-    rhs_scale = qarray.transpose_array(rhs_scale, (0, None, 1))
-    out = qarray.call_with_generic_broadcast(jnp.multiply, out, rhs_scale)
-  return out.astype(result_type)
 
 
 @interception.disable_interceptions
@@ -131,42 +75,33 @@ def ragged_dot_qt_bwd(
   """Backward pass for ragged_dot_qt custom VJP."""
   (lhs, rhs, group_sizes) = residuals  # lhs [M, K], rhs [G, K, N], g [M, N]
 
-  # dlhs = ragged_dot_general(g, rhs), dims are [M, K] = [M, N] @ [G, K, N]
-  dlhs_dnums = jax.lax.RaggedDotDimensionNumbers(
-      dot_dimension_numbers=(((1,), (2,)), ((), ())),
-      lhs_ragged_dimensions=[0],
-      rhs_group_dimensions=[0],
-  )
+  # dlhs = ragged_dot(g, rhs.swapaxes(1, 2))
+  # [M, K] = [M, N] @ [G, N, K]
+  # Transpose rhs as chlo.ragged_dot supports limited DimensionNumbers variants.
   g_for_dlhs = g
+  rhs = rhs.swapaxes(1, 2)  # [G, N, K]
   if config.dlhs_grad_qtype:
     if isinstance(rhs, qarray.QArray):
       assert rhs.zero_point is None
       # Support channelwise only on non-group and non-contracting axes.
-      g_for_dlhs = g * rhs.scale.squeeze(axis=0)  # [M, N] * [1, N]
-      rhs_for_dlhs = rhs.qvalue
-    else:
-      rhs_for_dlhs = rhs
+      g_for_dlhs = g * rhs.scale.squeeze(axis=2)  # [M, N] * [1, N]
+      rhs = rhs.qvalue
     g_how = qarray.HowToQuantize(
         qtype=config.dlhs_grad_qtype,
         channelwise_axes=[0],  # [M, N]
     )
     g_for_dlhs = qarray.quantize(g_for_dlhs, g_how)
-  else:
-    # If dlhs_grad_qtype is None, calculate dlhs in fp.
-    rhs_for_dlhs = (
-        qarray.dequantize(rhs) if isinstance(rhs, qarray.QArray) else rhs
-    )
-  dlhs = _ragged_dot_general(
+  dlhs = ragged_dot.ragged_dot(
       g_for_dlhs,
-      rhs_for_dlhs,
+      rhs,
       group_sizes,
-      dimension_numbers=dlhs_dnums,
       precision=precision,
       preferred_element_type=preferred_element_type,
       group_offset=group_offset,
   )
 
-  # drhs = ragged_dot_general(lhs, g), dims are [G, K, N] = [M, K] @ [M, N]
+  # drhs = ragged_dot_general(lhs, g)
+  # [G, K, N] = [M, K] @ [M, N]
   drhs_dnums = jax.lax.RaggedDotDimensionNumbers(
       dot_dimension_numbers=(((0,), (0,)), ((), ())),
       lhs_ragged_dimensions=[0],
@@ -177,21 +112,14 @@ def ragged_dot_qt_bwd(
     if isinstance(lhs, qarray.QArray):
       assert lhs.zero_point is None
       g_for_drhs = g * lhs.scale  # [M, N] * [M, 1]
-      lhs_for_drhs = lhs.qvalue
-    else:
-      lhs_for_drhs = lhs
+      lhs = lhs.qvalue
     g_how = qarray.HowToQuantize(
         qtype=config.drhs_grad_qtype,
         channelwise_axes=[1],  # [M, N]
     )
     g_for_drhs = qarray.quantize(g_for_drhs, g_how)
-  else:
-    # If drhs_grad_qtype is None, calculate drhs in fp.
-    lhs_for_drhs = (
-        qarray.dequantize(lhs) if isinstance(lhs, qarray.QArray) else lhs
-    )
-  drhs = _ragged_dot_general(
-      lhs_for_drhs,
+  drhs = ragged_dot.ragged_dot_general(
+      lhs,
       g_for_drhs,
       group_sizes,
       dimension_numbers=drhs_dnums,
