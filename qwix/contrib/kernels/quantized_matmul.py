1d99557feefe33ec7b6913e6b94d8399e4d4c041
James Chapman <chapmanjames@google.com>
2026-05-08 16:19:08 -0700

    Update quantized matmul kernel to optimize performance for fp32.
    PiperOrigin-RevId: 912746475

--- qwix/contrib/kernels/quantized_matmul.py
+++ qwix/contrib/kernels/quantized_matmul.py
@@ -11,25 +11,42 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Implements a quantized matmul kernel."""
+"""Implements a quantized matmul kernel.
+
+Info:
+This is being actively developed and features will be added over time.
+
+Terminology:
+- tile: refers to the sub-tensor that a scale corresponds to.
+- block: refers to the sub-tensor that pallas loads into vmem.
+
+Future work:
+- Multi-device kernels
+- Additional dtype support
+- Testing and benchmarking on all TPU generations (focused on TPUv5 for now)
+- Integration into the main qwix library
+"""
 
 import dataclasses
+import functools
 from typing import Any
 
 import jax
 import jax.experimental.pallas as pl
+import jax.experimental.pallas.tpu as pltpu
 import jax.numpy as jnp
 from qwix._src.core import qarray
 
-# TODO(chapmanjames): Update flag to False for production use.
-INTERPRET: bool = True
+_CORE_AXIS_NAME = 'core'
 
 
 @dataclasses.dataclass
 class QuantizedMatmulConfig:
-  bm: int = 128
-  bk: int = 128
-  bn: int = 128
+  """Configuration for the quantized matmul kernel determined by TPUv5 sweep."""
+
+  bm: int = 256
+  bk: int = 512
+  bn: int = 1024
   dtype: jnp.dtype = jnp.float32
 
 
@@ -37,28 +54,23 @@ def can_use_qmm(x, sx, y, sy, *, bm, bk, bn):
   """Returns whether the quantized matmul can be used."""
   mdim, kdim = x.shape
   _, ndim = y.shape
-  k_tiles = sx.shape[1]
 
   if mdim % bm != 0 or ndim % bn != 0 or kdim % bk != 0:
     # Block size must divide matrix size.
     return False
-  grid = (mdim // bm, ndim // bn, kdim // bk)
 
-  # k information
-  k_tile_size = kdim // k_tiles
-  if k_tile_size != bk:
-    # Block size must match the tile size for the reduction axis.
-    return False
+  # Check the scales reduction axis
   if sx.shape[1] != sy.shape[0]:
-    # Number of tiles must match for the scales.
+    # Number of tiles must match for the scales on the reduction axis.
     return False
 
-  if sx.shape[0] != grid[0] and sx.shape[0] != 1:
-    # Scale size must match grid size or be 1.
+  # Scale tile sizes are sufficiently large.
+  sm, sk, sn = sx.shape
+  if mdim // sm < 128 or kdim // sk < 128 or ndim // sn < 128:
     return False
 
-  if sy.shape[1] != grid[1] and sy.shape[1] != 1:
-    # Scale size must match grid size or be 1.
+  # TODO(chapmanjames): Improve this for bfloat16 scales.
+  if sx.dtype != jnp.float32 or sy.dtype != jnp.float32:
     return False
 
   return True
@@ -106,81 +118,221 @@ def can_use_qmm_in_dot_general(
   return True
 
 
-def quantized_matmul_kernel(x_ref, sx_ref, y_ref, sy_ref, o_ref):
-  @pl.when(pl.program_id(2) == 0)
-  def _():
-    o_ref[...] = jnp.zeros_like(o_ref)
-
-  o_ref[...] += (
-      jnp.matmul(x_ref[...], y_ref[...], preferred_element_type=o_ref.dtype)
-      * sx_ref[...]
-      * sy_ref[...]
-  )
-
-
-def q_matmul(x, sx, y, sy, *, bm=128, bk=128, bn=128, dtype=jnp.float32):
-  """Computes a quantized matmul with support for subchannel quantization.
-
-  This kernel does not cover all cases. In particular, it requires that
-  the block sizes match the tile sizes, and that the scale sizes match the grid
-  size or be 1.
+def quantized_matmul_kernel(
+    x_hbm: jax.Ref,
+    sx_hbm: jax.Ref,
+    y_hbm: jax.Ref,
+    sy_hbm: jax.Ref,
+    o_hbm: jax.Ref,
+    accum_vmem: jax.Ref,
+    *,
+    bm: int,
+    bk: int,
+    bn: int,
+):
+  """Quantized matmul kernel.
 
   Args:
-    x: The left-hand side matrix.
-    sx: The left-hand side scales.
-    y: The right-hand side matrix.
-    sy: The right-hand side scales.
-    bm: The block size for the m dimension.
-    bk: The block size for the k dimension.
-    bn: The block size for the n dimension.
-    dtype: The data type of the output.
+    x_hbm: Reference to the LHS input array in hbm (m, k)
+    sx_hbm: Reference to the LHS scale array in hbm (sm_global, sk_global)
+    y_hbm: Reference to the RHS input array in hbm (k, n)
+    sy_hbm: Reference to the RHS scale array in hbm (sk_global, sn_global)
+    o_hbm: Reference to the output array in hbm (m, n)
+    accum_vmem: Reference to the accumulation buffer in vmem (bm, bn)
+    bm: blockspec for the m dimension
+    bk: blockspec for the k dimension
+    bn: blockspec for the n dimension
+  """
+
+  sm_global, sk_global, *_ = sx_hbm.shape
+  sn_global = sy_hbm.shape[1]
+
+  # Grid
+  m, k = x_hbm.shape
+  _, n = y_hbm.shape
+  grid = (pl.cdiv(m, bm), pl.cdiv(n, bn), pl.cdiv(k, bk))
+
+  # Scale shapes within the kernel
+  sm = pl.cdiv(sm_global, grid[0])
+  sk = pl.cdiv(sk_global, grid[2])
+  sn = pl.cdiv(sn_global, grid[1])
+
+  # Blockspecs for the kernel
+  x_spec = pl.BlockSpec((bm, bk), lambda a, b, c: (a, c))
+  sx_spec = pl.BlockSpec((sm, sk, 1, 1), lambda a, b, c: (a, c, 0, 0))
+  y_spec = pl.BlockSpec((bk, bn), lambda a, b, c: (c, b))
+  sy_spec = pl.BlockSpec((sk, sn, 1, 1), lambda a, b, c: (c, b, 0, 0))
+  o_spec = pl.BlockSpec((bm, bn), lambda a, b, c: (a, b))
+
+  # Tile sizes corresponding to scale entries
+  m_tile_size = pl.cdiv(bm, sm)
+  k_tile_size = pl.cdiv(bk, sk)
+  n_tile_size = pl.cdiv(bn, sn)
+
+  # Kernel body
+  def quantized_matmul_body(
+      x_vmem: jax.Ref,
+      sx_vmem: jax.Ref,
+      y_vmem: jax.Ref,
+      sy_vmem: jax.Ref,
+      o_vmem: jax.Ref,
+  ):
+    kind = pl.program_id(2)
+
+    # Initialize accumulation buffer
+    @pl.when(kind == 0)
+    def _init():
+      accum_vmem[...] = jnp.zeros_like(accum_vmem)
+
+    for mloop in range(sm):
+      data_m_slc = pl.Slice(mloop * m_tile_size, m_tile_size)
+
+      for nloop in range(sn):
+        data_n_slc = pl.Slice(nloop * n_tile_size, n_tile_size)
+
+        # Loop over subchannel axis
+        for kloop in range(sk):
+          data_k_slc = pl.Slice(kloop * k_tile_size, k_tile_size)
+
+          # Load lhs and rhs
+          x = x_vmem[data_m_slc, data_k_slc]
+          y = y_vmem[data_k_slc, data_n_slc]
+
+          # Low Precision Matmul
+          xy = jnp.matmul(x, y, preferred_element_type=reduction_dtype)
+
+          # Access the scales we need
+          sx = sx_vmem[mloop, kloop, :, :]
+          sy = sy_vmem[kloop, nloop, :, :]
+
+          # Dequantize
+          xys = (xy * sx) * sy
+
+          # Accumulate results
+          accum_vmem[data_m_slc, data_n_slc] += xys
+
+    # Write results to output buffer.
+    @pl.when(pl.program_id(2) == pl.num_programs(2) - 1)
+    def _write():
+      o_vmem[...] = accum_vmem[...].astype(o_vmem.dtype)
+
+  # Set up accumulation buffer
+  reduction_dtype = jnp.int32
+
+  # Call the kernel.
+  pltpu.emit_pipeline(
+      quantized_matmul_body,
+      grid=grid,
+      in_specs=[x_spec, sx_spec, y_spec, sy_spec],
+      out_specs=o_spec,
+      core_axis_name=_CORE_AXIS_NAME,
+      dimension_semantics=(pltpu.PARALLEL, pltpu.PARALLEL, pltpu.ARBITRARY),
+  )(x_hbm, sx_hbm, y_hbm, sy_hbm, o_hbm)
+
+
+def quantized_matmul(
+    x: jax.Array,
+    sx: jax.Array,
+    y: jax.Array,
+    sy: jax.Array,
+    *,
+    bm: int,
+    bk: int,
+    bn: int,
+    accum_dtype=jnp.float32,
+    dtype,
+):
+  """Computes a quantized matmul using Pallas.
+
+  This function implements a quantized matmul kernel using Pallas. It supports
+  2d qvalue arrays and 2d scale arrays. If the scales are 1d, please ensure that
+  you expend them along the appropriate axis to match the required 2d shapes.
+  e.g. if sx is (sm,) then convert it to (sm, 1). If sx is (sk,) then convert it
+  to (1, sk).
+
+  This currently only support float32 scales.
+
+  Args:
+    x: The LHS input array (m, k)
+    sx: The LHS scale array (sm, sk)
+    y: The RHS input array (k, n)
+    sy: The RHS scale array (sk, sn)
+    bm: blockspec for the m dimension
+    bk: blockspec for the k dimension
+    bn: blockspec for the n dimension
+    accum_dtype: The dtype of the accumulation buffer
+    dtype: The dtype of the output array
 
   Returns:
-    The quantized matmul.
+    The result of the quantized matmul (m, n)
   """
-  mdim, kdim = x.shape
-  _, ndim = y.shape
-  k_tiles = sx.shape[1]
 
-  # Block specs for x and y.
-  assert mdim % bm == 0, f'Block size must divide matrix size,  {mdim=} {bm=}'
-  assert ndim % bn == 0, f'Block size must divide matrix size,  {ndim=} {bn=}'
-  assert kdim % bk == 0, f'Block size must divide matrix size,  {kdim=} {bk=}'
-  grid = (mdim // bm, ndim // bn, kdim // bk)
-  x_blockspec = pl.BlockSpec((bm, bk), lambda a, b, c: (a, c))
-  y_blockspec = pl.BlockSpec((bk, bn), lambda a, b, c: (c, b))
+  m, k = x.shape
+  _, n = y.shape
 
-  # k information
-  k_tile_size = kdim // k_tiles
-  assert k_tile_size == bk, (
-      'Block size must match the tile size for the reduction axis'
-      f' {k_tile_size=} {bk=}'
-  )
-  assert sx.shape[1] == sy.shape[0], 'Number of tiles must match for the scales'
+  # Check blockspecs divide matrix dimensions.
+  if m % bm != 0:
+    raise ValueError(f'Block size must divide matrix size,  {m=} {bm=}')
+  if n % bn != 0:
+    raise ValueError(f'Block size must divide matrix size,  {n=} {bn=}')
+  if k % bk != 0:
+    raise ValueError(f'Block size must divide matrix size,  {k=} {bk=}')
 
-  # m information
-  if sx.shape[0] == 1:
-    sx_blockspec = pl.BlockSpec((1, 1), lambda a, b, c: (0, c))
-  else:
-    assert (
-        sx.shape[0] == grid[0]
-    ), f'Scale size must match grid size,  {sx.shape[0]=} {grid[0]=}'
-    sx_blockspec = pl.BlockSpec((1, 1), lambda a, b, c: (a, c))
+  # Check matrix reduction axis
+  if x.shape[1] != y.shape[0]:
+    raise ValueError(
+        f'Matrix shapes must match for the reduction axis, {x.shape[1]=}'
+        f' {y.shape[0]=}'
+    )
 
-  # n information
-  if sy.shape[1] == 1:
-    sy_blockspec = pl.BlockSpec((1, 1), lambda a, b, c: (c, 0))
-  else:
-    assert (
-        sy.shape[1] == grid[1]
-    ), f'Scale size must match grid size,  {sy.shape[1]=} {grid[1]=}'
-    sy_blockspec = pl.BlockSpec((1, 1), lambda a, b, c: (c, b))
+  # Check scale reduction axis
+  if sx.shape[1] != sy.shape[0]:
+    raise ValueError(
+        f'Scale shapes must match for the reduction axis, {sx.shape[1]=}'
+        f' {sy.shape[0]=}'
+    )
 
-  return pl.pallas_call(
+  # Pre-process the scales
+  min_size = 128
+  sx = jnp.expand_dims(sx, axis=(-1, -2))
+  sy = jnp.expand_dims(sy, axis=(-1, -2))
+
+  # Tile sizes corresponding to scale entries
+  # e.g. shape of the submatrix in x or y that each scale entry covers.
+  m_tile_size = m // sx.shape[0]
+  k_tile_size = k // sx.shape[1]
+  n_tile_size = n // sy.shape[1]
+
+  # Expand scale shapes to make scale indexing easier
+  if m_tile_size > min_size:
+    sx = jnp.repeat(sx, repeats=m_tile_size // min_size, axis=0)
+  if k_tile_size > min_size:
+    sx = jnp.repeat(sx, repeats=k_tile_size // min_size, axis=1)
+    sy = jnp.repeat(sy, repeats=k_tile_size // min_size, axis=0)
+  if n_tile_size > min_size:
+    sy = jnp.repeat(sy, repeats=n_tile_size // min_size, axis=1)
+
+  # Create the tensor core mesh
+  tc_mesh = pltpu.create_tensorcore_mesh(axis_name=_CORE_AXIS_NAME)
+
+  # Create the output type
+  out_type = jax.core.ShapedArray((x.shape[0], y.shape[1]), dtype)
+
+  # Create the kernel with kwargs
+  kernel = functools.partial(
       quantized_matmul_kernel,
-      out_shape=jax.ShapeDtypeStruct((mdim, ndim), dtype),
-      grid=grid,
-      in_specs=(x_blockspec, sx_blockspec, y_blockspec, sy_blockspec),
-      out_specs=pl.BlockSpec((bm, bn), lambda a, b, c: (a, b)),
-      interpret=INTERPRET,
-  )(x, sx, y, sy).astype(dtype)
+      bm=bm,
+      bk=bk,
+      bn=bn,
+  )
+
+  if sx.dtype != jnp.float32 or sy.dtype != jnp.float32:
+    raise ValueError(f'Scales must be float32, {sx.dtype=} {sy.dtype=}')
+
+  # Call the kernel
+  return pl.kernel(
+      kernel,
+      out_type=out_type,
+      mesh=tc_mesh,
+      scratch_types=[pltpu.VMEM((bm, bn), accum_dtype)],
+  )(x, sx, y, sy)
--- tests/contrib/kernels/quantized_matmul_test.py
+++ tests/contrib/kernels/quantized_matmul_test.py
@@ -1,53 +1,71 @@
+from absl.testing import absltest
 from absl.testing import parameterized
+import jax
 import jax.numpy as jnp
-from qwix._src.core import dot_general
-from qwix._src.core import qarray
+import numpy as np
 from qwix.contrib.kernels import quantized_matmul
-from google3.testing.pybase import googletest
 
 
-class QuantizedMatmulTest(parameterized.TestCase, googletest.TestCase):
+class QuantizedMatmulTest(parameterized.TestCase):
 
+  @staticmethod
+  def jax_answer(x: jax.Array, sx: jax.Array, y: jax.Array, sy: jax.Array):
+    m, k = x.shape
+    _, n = y.shape
+    sm, sk = sx.shape
+    _, sn = sy.shape
+    x = x.reshape(sm, m // sm, sk, k // sk)
+    y = y.reshape(sk, k // sk, sn, n // sn)
+
+    # int matmul
+    xy = jnp.einsum("abcd,cdef->abcef", x, y, preferred_element_type=jnp.int32)
+
+    # multiply scales with the matmul result.
+    xys = jnp.einsum(
+        "abcef,ac,ce->abef", xy, sx, sy, preferred_element_type=jnp.float32
+    )
+    return xys.reshape(m, n)
+
+  @staticmethod
+  def generate_quantized_arrays(
+      data_shape, scale_shape, dtype, *, key: jax.Array
+  ) -> tuple[jax.Array, jax.Array]:
+    k1, k2 = jax.random.split(key)
+    x = jax.random.randint(
+        k1, data_shape, minval=-120, maxval=120, dtype=jnp.int8
+    )
+    sx = jax.random.normal(k2, scale_shape, dtype=dtype)
+    return x, sx
+
+  # TODO(chapmanjames): Add bfloat16 tests when implemented.
   @parameterized.parameters(
-      ((4, 8), (8, 16), 4, 16, 1, 1),
-      ((8, 16), (16, 32), 8, 32, 2, 2),
-      ((4, 4), (4, 4), 2, 2, 2, 2),
+      (1024, 1024, 1024, 8, 8, 8, 256, 512, 1024, jnp.float32),
+      (256, 512, 1024, 2, 4, 8, 128, 128, 128, jnp.float32),
+      (256, 512, 1024, 1, 1, 1, 256, 512, 1024, jnp.float32),
   )
-  def test_kernel_dot_general(self, lhs_shape, rhs_shape, bm, bn, bk, tile_k):
-    lhs = jnp.ones(lhs_shape, jnp.float32)
-    rhs = jnp.ones(rhs_shape, jnp.float32)
-
-    # Channelwise on axis 1 (contracting)
-    lhs_how = qarray.HowToQuantize(
-        qtype=jnp.int8,
-        tiled_axes={1: tile_k},
+  def test_kernel_dot_general(self, m, k, n, sm, sk, sn, bm, bk, bn, dtype):
+    key1, key2 = jax.random.split(jax.random.key(0))
+    lhs = QuantizedMatmulTest.generate_quantized_arrays(
+        (m, k), (sm, sk), dtype, key=key1
     )
-    # Channelwise on axis 0 (contracting)
-    rhs_how = qarray.HowToQuantize(
-        qtype=jnp.int8,
-        tiled_axes={0: tile_k},
+    rhs = QuantizedMatmulTest.generate_quantized_arrays(
+        (k, n), (sk, sn), dtype, key=key2
     )
 
-    q_lhs = qarray.quantize(lhs, lhs_how)
-    q_rhs = qarray.quantize(rhs, rhs_how)
-
-    kernel_answer = quantized_matmul.q_matmul(
-        q_lhs.qvalue,
-        q_lhs.scale,
-        q_rhs.qvalue,
-        q_rhs.scale,
+    kernel_answer = quantized_matmul.quantized_matmul(
+        lhs[0],
+        lhs[1],
+        rhs[0],
+        rhs[1],
         bm=bm,
-        bn=bn,
         bk=bk,
+        bn=bn,
+        dtype=dtype,
     )
 
-    qwix_answer = dot_general.dot_general(
-        q_lhs,
-        q_rhs,
-        (([1], [0]), ([], [])),
-    )
-    self.assertTrue(jnp.allclose(kernel_answer, qwix_answer))
+    qwix_answer = QuantizedMatmulTest.jax_answer(lhs[0], lhs[1], rhs[0], rhs[1])
+    np.testing.assert_allclose(kernel_answer, qwix_answer, rtol=1e-2, atol=1e-2)
 
 
 if __name__ == "__main__":
-  googletest.main()
+  absltest.main()
